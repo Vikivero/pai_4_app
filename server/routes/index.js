a60b3ec84ae4f5bb1e45f9e2fb99e3e34e12afa6
vikivero <vikichan.ch@gmail.com>
2023-06-22 18:50:49 +0200

    dodano tailwinds

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/api', function(req, res, next) {
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200)
+
+  res.json({
+    'name': 'Natalia',
+    'msg': 'hello!'
+  });
 });
 
 module.exports = router;
